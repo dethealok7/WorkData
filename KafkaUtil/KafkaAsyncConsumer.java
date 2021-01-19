069ac885dc51a2f6c288d1ffd89669752cbadb34
alok <musykworld@gmail.com>
2021-01-19 15:50:50 +0530

    changes

--- KafkaUtil/KafkaAsyncConsumer.java
+++ KafkaUtil/KafkaAsyncConsumer.java
@@ -9,21 +9,22 @@ import org.springframework.context.annotation.Conditional;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-/** @author alokdethe*/
+/* @author alokdethe
+ */
 @Component
-@Conditional(ConsumerAutoCommitAsync.class)
+@Conditional(ConsumerAutoAsync.class)
 public class KafkaAsyncConsumer {
     
     @Autowired
-    private KafkaIntegration kafkaMessagePro;
+    private KafkaIntegrator kafkaMessageProcessor;
     
     @Autowired
-    private ConsumerRecordKafkaMsgMap consumerRecordKafkaMsgMap;
+    private ConsumerRecordKafkaMessageMapper consumerRecordKafkaMessageMapper;
     
     @KafkaListener(topics = "topicname")
     @Conditional(ConsumerAutoCommitAsync.class)
     public void receive(ConsumerRecord<String, byte[]> consumerRecord){
-        kafkaMessagePro.process(consumerRecordKafkaMsgMap.getKafkaMessage(consumerRecord, null), consumerRecord.value());
+        kafkaMessageProcessor.process(consumerRecordKafkaMessageMapper.getKafkaMessage(consumerRecord, null), consumerRecord.value());
     }
     
 }
