7cee3c2f6b9b7f7ab4354a6b7d71df920a8ef284
alok <musykworld@gmail.com>
2021-01-19 15:57:12 +0530

    changes

--- KafkaUtil/KafkaAsyncConsumer.java
+++ KafkaUtil/KafkaAsyncConsumer.java
@@ -2,29 +2,31 @@
 package com.poc.restapi;
 
 
-import com.treemode.kafkaintegrator.kafkaMessage.ConsumerRecordKafkaMessageMapper;
+import com.treemode.kafkaintegrator.kafkaMessage.ConsumerRecordKafkaMsgMap;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-/* @author alokdethe
+/**
+ *
+ * @author alokdethe
  */
 @Component
 @Conditional(ConsumerAutoAsync.class)
 public class KafkaAsyncConsumer {
     
     @Autowired
-    private KafkaIntegrator kafkaMessageProcessor;
+    private KafkaIntegrate kafkaMsgPro;
     
     @Autowired
-    private ConsumerRecordKafkaMessageMapper consumerRecordKafkaMessageMapper;
+    private ConsumerRecordKafkaMsgMap consumerRecordKafkaMsgMap;
     
     @KafkaListener(topics = "topicname")
-    @Conditional(ConsumerAutoCommitAsync.class)
+    @Conditional(ConsumerAutoAsync.class)
     public void receive(ConsumerRecord<String, byte[]> consumerRecord){
-        kafkaMessageProcessor.process(consumerRecordKafkaMessageMapper.getKafkaMessage(consumerRecord, null), consumerRecord.value());
+        kafkaMsgPro.process(consumerRecordKafkaMsgMap.getKafkaMessage(consumerRecord, null), consumerRecord.value());
     }
     
 }
